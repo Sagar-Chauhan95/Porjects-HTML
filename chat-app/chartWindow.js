290187de5e2eacd832d2bef0691e308f49e772e1
Sagar Chauhan <schauhan@miu.edu>
2024-06-29 15:17:54 -0500

    html projects

---
+++ chat-app/chartWindow.js
@@ -0,0 +1,62 @@
+
+
+let activeUser = ""; // Initialize the active user
+
+// Read user names from local storage
+const firstUserName = localStorage.getItem('first-User');
+const secondUserName = localStorage.getItem('second-user');
+
+// Set user names on the buttons
+document.getElementById('sagarBtn').innerHTML = firstUserName;
+document.getElementById('sahrfearBtn').innerHTML = secondUserName;
+
+function setActiveUser(user) {
+    activeUser = user; // Set the active user to the selected user
+    let activeUserElement = document.getElementById('active-user');
+    activeUserElement.style.color = "black";
+    activeUserElement.style.textShadow = "1px 12px 9px rgba(0,0,0,0.6)";
+    activeUserElement.style.fontWeight = "bold";
+    activeUserElement.style.letterSpacing = "3px";
+
+    activeUserElement.innerHTML = user + " is typing...";
+}
+
+
+
+function sendMessage() {
+    let typingArea = document.getElementById('mainText');
+    let typingMessage = document.getElementById('type-message');
+
+    if (typingMessage.value) {
+        let messageDiv = document.createElement('div');
+        messageDiv.style.marginTop = '10px'; // Add some space between messages
+
+        let p = document.createElement('p');
+
+        if (activeUser === firstUserName) {
+            p.innerHTML = activeUser + ': ' + typingMessage.value;
+            p.style.color = "white";
+            p.style.background = "blue";
+            p.style.borderRadius = "10px";
+            p.style.textAlign = "left";
+            p.style.padding = "5px";
+     
+        } else if (activeUser === secondUserName) {
+            p.innerHTML = activeUser + ': ' + typingMessage.value;
+            p.style.color = "white";
+            p.style.background = "#343A3F";
+            p.style.borderRadius = "10px";
+            p.style.textAlign = "right";
+            p.style.padding = "5px";   
+        }
+        messageDiv.appendChild(p);
+        typingArea.appendChild(messageDiv);
+    }
+    typingMessage.value='';
+}
+// Clear the chat
+function clearChat(){
+    let clearChat = document.getElementById('mainText');
+    clearChat.innerHTML = '';
+  
+  }
---
+++ chat-app/login.js
@@ -0,0 +1,72 @@
+
+const form = document.getElementById('form');
+const firstName = document.getElementById('loginFirstName');
+const secondName = document.getElementById('loginLastName');
+const password = document.getElementById('loginPassword');
+const storedFirstName = localStorage.getItem('first-name')
+const storedSecondName = localStorage.getItem('last-name');
+const storedPassword = localStorage.getItem('valid-password');
+
+form.addEventListener('submit', (e) =>{
+    validateInputs();
+    const firstUser = firstName.value;
+    const secondUser = secondName.value;
+    if(isFormValid() == true){
+        form.submit();
+        localStorage.setItem('first-User',firstUser);
+        localStorage.setItem('second-user', secondUser);
+
+    }else{
+        e.preventDefault();  
+    }
+})
+
+function isFormValid() {
+    const inputContainers = form.querySelectorAll('.form_group');
+    let result = true;
+    inputContainers.forEach((container)=>{
+        if(container.classList.contains('error')){
+            result =false;
+        }
+    })
+    return result;
+}
+
+const setError =(element,message) =>{
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+};
+const setSuccess = (element) =>{
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    errorDisplay.innerText =' ';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const validateInputs = () => {
+    const firstNameValue = firstName.value.trim();
+    const secondNameValue = secondName.value.trim();
+    const passwordValue = password.value.trim();
+
+    if(firstNameValue !== storedFirstName){
+        setError(firstName,'invalid firstname');
+    }else{
+        setSuccess(firstName);
+    }
+
+    if(secondNameValue!==storedSecondName){
+        setError(secondName,'invalid secondname');   
+    }else{
+        setSuccess(secondName);
+    }
+
+    if(passwordValue !== storedPassword){
+        setError(password,"Invalid password");
+    }else{
+        setSuccess(password);
+    }
+};
